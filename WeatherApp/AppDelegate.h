9cd7fd74dbe3db6a12d6a88c4a6be6ab18f614b4
Ignacio Mariani <ignaciom6@hotmail.com>
2016-07-18 15:08:40 -0300

    Initial Commit

---
+++ WeatherApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherApp
+//
+//  Created by Ignacio on 18/7/16.
+//  Copyright © 2016 Ignacio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherApp
+//
+//  Created by Ignacio on 18/7/16.
+//  Copyright © 2016 Ignacio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
